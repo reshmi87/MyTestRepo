766b7ee4311ab9746199eb9e7608226464642a79
Reshmi <r.reshmi87@gmail.com>
2025-01-27 15:49:32 -0800

    First commit

---
+++ src/test/java/StepDefinition/tree.java
@@ -0,0 +1,25 @@
+package StepDefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class tree {
+	@Given("User is in the Home page after logging in")
+	public void user_is_in_the_home_page_after_logging_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User clicks on Get Started button under the section Tree")
+	public void user_clicks_on_get_started_button_under_the_section_tree() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("User lands on the Tree page")
+	public void user_lands_on_the_tree_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+}
---
+++ src/test/resources/features/tree.feature
@@ -0,0 +1,6 @@
+Feature: Testing the Tree Module in dsAlgo Portal
+
+ Scenario: Verify that the user is able to land in Tree page
+ Given User is in the Home page after logging in
+ When User clicks on Get Started button under the section Tree
+ Then User lands on the Tree page
